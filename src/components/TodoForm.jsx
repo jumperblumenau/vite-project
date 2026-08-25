95ec222c3cb6b0bb2021573dfe58954b2ac73eb0
hericota <henriquegdall@gmail.com>
2026-08-25 19:00:44 -0300

    refactor: improve formatting and structure of TodoForm component

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,25 +1,37 @@
-import {useTarefaStore} from '../store/useTarefaStore';
+import { useTarefaStore } from "../store/useTarefaStore";
 function TodoForm({ todo }) {
+  const { setTodos } = useTarefaStore();
 
-    const { setTodos } = useTarefaStore();
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const newTodo = {
-            id: Date.now(),
-            text: e.target[0].value,
-            completed: false,
-        };
-        setTodos([...todo, newTodo]);
-        e.target.reset();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newTodo = {
+      id: Date.now(),
+      text: e.target[0].value,
+      completed: false,
     };
+    setTodos([...todo, newTodo]);
+    e.target.reset();
+  };
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <input type="text" placeholder="Add todo" />
-            <button type="submit">Add</button>
-        </form>
-    );
+  return (
+    <div
+      className="form"
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: "20px",
+        border: "1px solid #ccc",
+        padding: "10px",
+        borderRadius: "5px",
+      }}
+    >
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Add homeworks" />
+        <button type="submit">Add</button>
+      </form>
+    </div>
+  );
 }
 
-export default TodoForm
+export default TodoForm;
