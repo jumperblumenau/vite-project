c12ebb6d12d27da3af42ac0bfce7b168e68d8c1f
Sergio Herrera <59238381+Shkingone@users.noreply.github.com>
2026-09-15 23:15:24 +0000

    se creo el componente TodoForm

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,5 +1,41 @@
+import { useState } from "react";
 import { useTarefaStore } from "../store/useTarefaStore";
-import TodoList from "./TodoList";
+
 
 export default function TodoForm() {
-  
+  const [titulo, setTitulo] = useState('')
+  const [enviando, setEnviando] = useState(false)
+  const adicionarTarefa = useTarefaStore((estado) => estado.adicionarTarefas)
+
+  async function aoEnviar(evento) {
+    evento.preventDefault()
+
+    if (!titulo.trim() || enviando) return
+
+    setEnviando(true)
+    await adicionarTarefa(titulo)
+    setTitulo('')
+    setEnviando(false)
+    
+  }
+return (
+
+    <form className="formulario-tarefa" onSubmit={aoEnviar}>
+        <input 
+        type="text"
+        className="formulario_tarefa__input"
+        placeholder="O que voce precisa fazer?"
+        value={titulo}
+        onChange={(evento) => setTitulo(evento.target.value)}
+        aria-label="Titulo da nova tarefa"/>
+
+        <button
+        type="submit"
+        className="formulario_tarefa__botao"
+        disabled= {!titulo.trim() || enviando}>
+            {enviando? 'Adicionando...' : 'Adicionar'}
+            
+        </button>
+    </form>
+)
+}
